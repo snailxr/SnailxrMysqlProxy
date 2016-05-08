7542cb418a919a29be026f0a2afb94082fd018a5
snailxr <snailxr@192.168.1.103>
2016-05-08 12:43:42 +0800

    first commit

---
+++ src/main/java/org/snailxr/SnailxrMysqlProxy/App.java
@@ -0,0 +1,13 @@
+package org.snailxr.SnailxrMysqlProxy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
